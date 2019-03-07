7496a8cc380b20561053783a15f1f61969c80c3f
Dansranbavuu Samdan <16b1seas2873@stud.num.edu.mn>
2019-03-06 20:30:19 -0800

    hw2 last

--- os-master/Homework/HW_2/List/LinkedList.c
+++ os-master/Homework/HW_2/List/LinkedList.c
@@ -1,20 +1,17 @@
-#include <linux/module.h>   /* Needed by all modules  */
-#include <linux/kernel.h>   /* Needed for KERN_INFO   */
-#include <linux/init.h>     /* Needed for the macros  */
-#include <linux/list.h>     /* Needed for linked list */
-#include <linux/types.h>    /* Needed for list macros */
-#include <linux/slab.h>     /* Needed for Kernel */
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/list.h>
+#include <linux/types.h>
+#include <linux/slab.h>
 
 #define NUM_OF_BIRTHDAYS    6
 
-/* Birthday struct  */
-typedef struct _birthday {
-    
+typedef struct _birthday {    
     int day;
     int month;
     int year;
     struct list_head list;
-
 } birthday ;
 
 LIST_HEAD(birthday_list);
@@ -30,32 +27,22 @@ int birthdayList_init(void) {
         person->year = 1995 + i;
         INIT_LIST_HEAD(&person->list);
         list_add_tail(&person->list, &birthday_list);
-
     }
     birthday *ptr;
     list_for_each_entry(ptr, &birthday_list, list) {
-
-        printk(KERN_INFO "Birthday: Month %d Day %d Year %d\n",
-                ptr->month,
-                ptr->day,
-                ptr->year);
+        printk(KERN_INFO "Birthday: Month %d Day %d Year %d\n", ptr->month, ptr->day, ptr->year);
     }
-
-           return 0;
+    return 0;
 }
 
 void birthdayList_exit(void) {
-
     printk(KERN_INFO "Removing Module\n");
     birthday *ptr, *next;
     list_for_each_entry_safe(ptr, next, &birthday_list, list) {
         list_del(&ptr->list);
-        kfree(ptr);
-    
+        kfree(ptr);    
     }
-
-    printk(KERN_INFO "Memory free done\n");
-
+    printk(KERN_INFO "Removing birthday list and free momery!\n");
 }
 
 module_init(birthdayList_init);
